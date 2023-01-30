13d554bfa589751b814bb9e5a06ed161eb3ad08b
Ayaz Ayupov <ayaz@releaseband.com>
2023-01-30 11:34:46 +0700

    hw_08 #time 1h #comment implemented ReadDir func

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	ErrValueInvalid = errors.New("env value invalid")
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +20,69 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func NewEnv(value string) EnvValue {
+	var needRemove bool
+	if value == "" {
+		needRemove = true
+	}
+
+	return EnvValue{Value: value, NeedRemove: needRemove}
+}
+
+func clearString(str string) string {
+	str = strings.Replace(str, " ", "", -1)
+	str = strings.Replace(str, "\t", "", -1)
+	str = strings.Replace(str, "\\0", "\n", -1) // todo: ??????
+
+	return str
+}
+
+func ReadFirstLineInFile(name string) (string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return "", err
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan() // читаем только первую строку
+
+	resp := clearString(scanner.Text())
+
+	return resp, scanner.Err()
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
-func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+func ReadDir(name string) (Environment, error) {
+	files, err := os.ReadDir(name)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := make(Environment, len(files))
+
+	for _, file := range files {
+		if !file.IsDir() {
+			fileName := file.Name()
+			value, err := ReadFirstLineInFile(name + "/" + fileName)
+			if err != nil {
+				return nil, err
+			}
+
+			if strings.Contains(value, "=") {
+				return nil, ErrValueInvalid
+			}
+
+			resp[fileName] = NewEnv(value)
+		}
+
+	}
+
+	return resp, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,61 @@
 package main
 
-import "testing"
+import (
+	"io/fs"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	pathErr := func(t *testing.T, err error) {
+		_, ok := err.(*fs.PathError)
+		require.True(t, ok)
+	}
+
+	expEnvironments := Environment{
+		"BAR":   NewEnv("bar"),
+		"EMPTY": NewEnv(""),
+		"HELLO": NewEnv("Hello"),
+		"UNSET": NewEnv(""),
+		"FOO":   NewEnv("foo"),
+	}
+
+	tests := []struct {
+		name       string
+		checkError func(t *testing.T, err error)
+		dirName    string
+		withEnv    bool
+	}{
+		{
+			name:       "dir not exist",
+			checkError: pathErr,
+			dirName:    "ada",
+		},
+		{
+			name:       "is not dir",
+			checkError: pathErr,
+			dirName:    "env_reader.go",
+		},
+		{
+			name:    "success",
+			dirName: "testdata/env",
+			withEnv: true,
+			checkError: func(t *testing.T, err error) {
+				require.Nil(t, err)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadDir(tt.dirName)
+			tt.checkError(t, err)
+			if !tt.withEnv {
+				require.Nil(t, got)
+			} else {
+				require.Equal(t, len(expEnvironments), len(got))
+			}
+		})
+	}
 }
