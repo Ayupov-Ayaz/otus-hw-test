7cafbfd5655f56f70a7968173d25dbcb6d56367f
Ayaz Ayupov <ayaz@releaseband.com>
2023-04-17 18:39:32 +0700

    hw12 init

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,49 @@
 package main
 
+import "strconv"
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
 type Config struct {
 	Logger LoggerConf
+	HTTP   HTTPServerConf
 	// TODO
+}
+
+func DefaultConfig() Config {
+	return Config{
+		Logger: DefaultLoggerConf(),
+		HTTP:   DefaultHTTPServerConf(),
+	}
 }
 
 type LoggerConf struct {
 	Level string
-	// TODO
+}
+
+func DefaultLoggerConf() LoggerConf {
+	return LoggerConf{
+		Level: "debug",
+	}
+}
+
+type HTTPServerConf struct {
+	Port int
+}
+
+func DefaultHTTPServerConf() HTTPServerConf {
+	return HTTPServerConf{
+		Port: 8080,
+	}
+}
+
+func (c HTTPServerConf) PortToString() string {
+	return strconv.Itoa(c.Port)
 }
 
 func NewConfig() Config {
-	return Config{}
+	cfg := DefaultConfig()
+	// todo: unmarshal environment variables
+	return cfg
 }
-
-// TODO
